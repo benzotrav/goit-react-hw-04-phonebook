90cdbe7f5f2abae5870b9533997eacf7e7fbca25
benzotrav <93350704+benzotrav@users.noreply.github.com>
2022-08-11 21:34:19 +0100

    hook fix

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -5,30 +5,7 @@ import { Contacts } from './Contacts/Contacts';
 import { Box, Title } from './Bookform/Bookform-styled';
 import { useState, useEffect } from 'react';
 import Notiflix from 'notiflix';
-
-
-const LS_KEY = 'savedContacts';
-
-const setToLocalStorage = contacts => {
-  try {
-    localStorage.setItem(LS_KEY, JSON.stringify(contacts));
-  } catch (error) {
-    console.log(error);
-  }
-};
-
-const getFromLocalStoreage = () => {
-  try {
-    const contacts = localStorage.getItem(LS_KEY);
-    if (contacts) {
-      return JSON.parse(contacts);
-    } else {
-      return [];
-    }
-  } catch (error) {
-    console.log(error);
-  }
-};
+import { setToLocalStorage, getFromLocalStoreage } from 'utils/localStorageHooks';
 
 
 export const App = () => {
